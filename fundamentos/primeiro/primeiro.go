2044e6b9ee01ab3c4c830784bb89e5bdb14049a7
William de Araujo Almeida <estudoswill@gmail.com>
2026-02-17 10:33:25 -0300

    comandos, const, prints, tipos

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,24 @@
+//Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+
+
+import "fmt"
+
+// A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("primeiro ")
+	fmt.Print("Programa! ")
+
+	/*
+	Sobre comentários
+	- Priorize código legível e faça comentários para agregar valor!
+	- Evite comentários óbvios
+	*/
+
+
+}
